c10b5de81ea8757208b46de0edc9df5e58ea3fd9
JunoNgx <juno.ngx@gmail.com>
2020-06-24 03:13:37 +0800

    Updates

---
+++ precache-manifest.038dff88b56a0f81a3d367ed7e8491f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5910691239c7c8258cc2f299175b5180",
+    "url": "/reacttodolist/index.html"
+  },
+  {
+    "revision": "d061372584c81493ddad",
+    "url": "/reacttodolist/static/css/main.35094f4f.chunk.css"
+  },
+  {
+    "revision": "1437ff8d661219ffbd62",
+    "url": "/reacttodolist/static/js/2.605faf58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodolist/static/js/2.605faf58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d061372584c81493ddad",
+    "url": "/reacttodolist/static/js/main.680d7bbb.chunk.js"
+  },
+  {
+    "revision": "c4d72408979ee3c77c8d",
+    "url": "/reacttodolist/static/js/runtime-main.9be56d1c.js"
+  }
+]);
